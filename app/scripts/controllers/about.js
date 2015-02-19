cde01bea35f2acaa07e0da4dc51ed3b46dd77292
Brandon Thepvongsa <brandonthepvongsa@gmail.com>
2015-02-18 22:50:31 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pictureProjectorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pictureProjectorApp
+ */
+angular.module('pictureProjectorApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pictureProjectorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pictureProjectorApp
+ */
+angular.module('pictureProjectorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
